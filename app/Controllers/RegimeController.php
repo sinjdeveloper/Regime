ecc449c2d40512b85e6488ff837e48ce70a03df2
sinjdeveloper <sandamamison1@gmail.com>
2026-05-06 10:22:57 +0300

    Creation des classes models et controller

---
+++ app/Controllers/RegimeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class RegimeController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
